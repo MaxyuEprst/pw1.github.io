6feb63dc0ead2c5805df00b892500fffd72ecd3d
maxyu <num_one_id@vk.com>
2025-06-01 17:59:15 +0300

    lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,1 @@
-import { useState } from 'react'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-        <ul>
-          <li>первый</li> 
-          <li>второй</li> 
-          <li>третий</li> 
-          <li>четверый</li>  
-        </ul>
-    </>
-  )
-}
-
-export default App
---
+++ src/MyInfo.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { useState } from 'react'
+
+function MyInfo() {
+  return (
+    <>
+        <h1>Maxyu Eprst</h1>
+        <p>хахах я люблю писюны ахахах огромные</p>
+        <h3>Хочу посетить:</h3>
+        <ul>
+            <li>чебоксары</li>
+            <li>чебоксары</li>
+            <li>чебоксары</li>
+        </ul>
+    </>
+  )
+}
+
+export default MyInfo
